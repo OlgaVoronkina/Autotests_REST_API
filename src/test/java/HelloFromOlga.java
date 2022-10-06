2379784253283594736b123ba6b928a1f8949a4e
OlgaVoronkina <voronkina.olga.s@gmail.com>
2022-10-06 17:49:06 +0300

    hello from Olga hello from Olga

---
+++ src/test/java/HelloFromOlga.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloFromOlga {
+    @Test
+    public void hello() {
+        String name = "Olga";
+        System.out.println("Hello from " + name);
+    }
+}
--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,8 +1,19 @@
+import  io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorld {
     @Test
     public void testHelloWorld(){
         System.out.println("Hello world");
+    }
+
+    @Test
+    public void testHelloWorld2(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+
     }
 }
