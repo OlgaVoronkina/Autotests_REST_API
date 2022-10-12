b67002466e73fa16624753d512e5653678163cc9
OlgaVoronkina <voronkina.olga.s@gmail.com>
2022-10-12 23:57:28 +0300

    Ex 6

---
+++ src/test/java/Ex6.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex6 {
+    @Test
+    public void longRedirect(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("location");
+        System.out.println(locationHeader);
+    }
+
+}
