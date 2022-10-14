f14814e06035d6c8512c858adff5a4a7a5601d52
OlgaVoronkina <voronkina.olga.s@gmail.com>
2022-10-14 22:59:10 +0300

    Ex7

---
+++ src/test/java/Ex7.java
@@ -0,0 +1,44 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex7 {
+    @Test
+    public void longRedirect() {
+        String url= "https://playground.learnqa.ru/api/long_redirect";
+        do {
+          url=this.returnHeaderLocation(url);
+        }
+        while (this.returnCode(url)==301);
+    }
+
+        public String returnHeaderLocation(String url){
+            System.out.println("Base URL is " +url);
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(url)
+                    .andReturn();
+            url = response.getHeader("location");
+            System.out.println("Redirect URL is " +url);
+
+            return url;
+        }
+
+        public int returnCode(String url){
+            int code;
+            Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get(url)
+                .andReturn();
+            code = response.getStatusCode();
+            System.out.println(code);
+            return code;
+    }
+
+}
